8f3f4ff1d85430d13983f3475d374da2d9b8c68f
micogongob <15572400+micogongob@users.noreply.github.com>
2021-01-20 22:04:04 +0800

    change whitespace in handler

--- lambda/get-all-authors.js
+++ lambda/get-all-authors.js
@@ -1,5 +1,8 @@
 const AWS = require('aws-sdk');
-const DB = new AWS.DynamoDB({ region: process.env.AWS_REGION, apiVersion: '2012-08-10' });
+const DB = new AWS.DynamoDB({ 
+    region: process.env.AWS_REGION, 
+    apiVersion: '2012-08-10' 
+});
 
 exports.handler = async (event, context) => {
     const params = { TableName: process.env.AUTHORS_TABLE };
