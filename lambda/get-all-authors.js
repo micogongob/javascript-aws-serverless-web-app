08f562cb341d745fcc36b4cf04001b0626a5fc6f
micogongob <15572400+micogongob@users.noreply.github.com>
2021-01-20 22:21:40 +0800

    use flatten helper in get-all-authors function

--- lambda/get-all-authors.js
+++ lambda/get-all-authors.js
@@ -3,18 +3,14 @@ const DB = new AWS.DynamoDB({
     region: process.env.AWS_REGION, 
     apiVersion: '2012-08-10' 
 });
+const dynamoDbFlattenHelper = require('./helper/dynamodb-flatten-helper');
 
 exports.handler = async (event, context) => {
     const params = { TableName: process.env.AUTHORS_TABLE };
 
     try {
-        const data = await DB.scan(params).promise();
-
-        const authors = data.Items.map(item => { 
-            return { id: item.id.S, firstName: item.firstName.S, lastName: item.lastName.S };
-        });
-
-        return authors;
+        const authors = await DB.scan(params).promise();
+        return authors.Items.map(dynamoDbFlattenHelper.flattenDynamoDbItem);
     } catch (error) {
         console.log(error);
         return error;
