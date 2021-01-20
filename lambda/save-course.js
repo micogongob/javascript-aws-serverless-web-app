3dc82ddff543c54f9d50dd101332a3e74e43803b
micogongob <15572400+micogongob@users.noreply.github.com>
2021-01-20 22:04:50 +0800

    new lambda function that creates courses

---
+++ lambda/save-course.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const DB = new AWS.DynamoDB({ 
+    region: process.env.AWS_REGION, 
+    apiVersion: '2012-08-10' 
+});
+const dynamoDbFlattenHelper = require('./helper/dynamodb-flatten-helper');
+
+exports.handler = async (event, context) => {
+    const id = buildIdFromEvent(event);
+    const createParams = buildDbCreateParams(id, event);
+    
+    try {
+        await DB.putItem(createParams).promise();
+        return dynamoDbFlattenHelper.flattenDynamoDbItems(createParams);
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+function buildIdFromEvent({ title }) {
+    return replaceAll(title, ' ', '-').toLowerCase();
+}
+
+function buildDbCreateParams(id, { title, authorId, length, category }) {
+    return {
+      Item: {
+        id: {
+          S: id,
+        },
+        title: {
+          S: title,
+        },
+        watchHref: {
+          S: `http://www.pluralsight.com/courses/${id}`,
+        },
+        authorId: {
+          S: authorId,
+        },
+        length: {
+          S: length,
+        },
+        category: {
+          S: category,
+        },
+      },
+      TableName: process.env.COURSES_TABLE,
+    };
+}
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
