7d5d7d5f754bd1eed6add6e89876c5576db1efb1
micogongob <15572400+micogongob@users.noreply.github.com>
2021-01-21 15:45:27 +0800

    store cf stack name in env var

--- scripts/api-deploy.sh
+++ scripts/api-deploy.sh
@@ -6,8 +6,15 @@ then
   exit 1
 fi
 
+if [ -z $WEB_APP_STACK_NAME ]
+then
+  echo "WEB_APP_STACK_NAME env var missing"
+  exit 1
+fi
+
 bucket=$LAMBDA_ARTIFACTS_S3_BUCKET
+stackName=$WEB_APP_STACK_NAME
 
 aws cloudformation package --s3-bucket $bucket --template-file template.yml --output-template-file output.yml
 
-aws cloudformation deploy --stack-name aws-serverless-web-app  --template-file output.yml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --stack-name $stackName --template-file output.yml --capabilities CAPABILITY_NAMED_IAM
--- scripts/frontend-deploy.sh
+++ scripts/frontend-deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [ -z $WEB_APP_STACK_NAME ]
+then
+  echo "WEB_APP_STACK_NAME env var missing"
+  exit 1
+fi
+
+stackName=$WEB_APP_STACK_NAME
+
 webAppBucket=$(aws cloudformation describe-stacks \
-  --stack-name aws-serverless-web-app \
+  --stack-name $stackName \
   --query "Stacks[0].Outputs[?OutputKey=='WebAppS3Bucket'].OutputValue" \
   --output text)
 
